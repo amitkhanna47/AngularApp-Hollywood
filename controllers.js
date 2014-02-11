50ae90815e38c637167d5c18377f793122e992a0
Amit <amitkhanna1985@gmail.com>
2014-02-12 02:49:52 +0530

    new changes

--- controllers.js
+++ controllers.js
@@ -21,6 +21,18 @@ app.config(function($routeProvider){
 		}
 	)
 	.when(
+		"/latest/released/:categoryId/:moviId/posters",
+		{
+			action:"standard.latest.movi_details.posters"
+		}
+	)
+	.when(
+		"/latest/released/:categoryId/:moviId/castcrew",
+		{
+			action:"standard.latest.movi_details.cast"
+		}
+	)
+	.when(
 		"/credits",
 		{
 			action:"standard.credits"
@@ -33,7 +45,8 @@ app.config(function($routeProvider){
 	)
 });
 
-app.service("requestService",function(){
+app.service("requestService",function(){	
+
 	function setContext(action){
 		sections = action.split('.');			
 		//console.log(sections);
@@ -41,7 +54,25 @@ app.service("requestService",function(){
 	}	
 
 	function getSection(prefix){
-		if(prefix==undefined || prefix=="") return sections[0];		
+				
+		if(action.indexOf(prefix + '.') == -1)
+		{			
+			return;
+			
+		}
+	
+		if(prefix==undefined || prefix=="")
+		{
+			console.log(sections[0]); 
+			return sections[0];
+		}			
+		console.log(sections[prefix.split('.').length]);
+		
+		if(sections[prefix.split('.').length] == 'movi_details'){
+			global.anim = true;			
+			//console.log(global.anim);			
+		}
+		
 		return sections[prefix.split('.').length];		
 	}
 	
@@ -65,15 +96,17 @@ app.service("dataService",function($http){
 	var urlLink;
 	var sizer;
 	function setPath(data, url, size){	
+		sizer = "";
 		if(!urlLink) urlLink = url || "http://d3gtl9l2a4fn1j.cloudfront.net/t/p/";
 				
-		if(!sizer) sizer = size || "w185";				
-		var comUrl = urlLink + sizer;
-				
+		if(!sizer){			
+			sizer = size || "w185";			
+		} 
+		var comUrl = urlLink + sizer;				
 			
 		angular.forEach(data, function(val, key){
 			if(angular.isObject(val)){
-				setPath(data[key]);
+				setPath(data[key], null, size);
 			}
 			else
 			{
@@ -85,26 +118,29 @@ app.service("dataService",function($http){
 				}
 			}	
 		});
-		console.log(sizer);
-		sizer = "";						
+		//console.log(sizer);
+		//sizer = "";						
 		return data;				
 	}
 	
 	return {getData:getData, setPath:setPath};
 });
 
-app.controller('appCtrl',function($scope, $route, requestService, dataService){
-
-	$scope.$on("$requestContextChanged",function(){
-		$scope.subview = requestService.getSection();
-	});			
-
-
+app.controller('appCtrl',function($scope, $route, $rootScope, $routeParams, requestService, dataService){
+	global = $scope;
+	$scope.anim = false;
+	
 	$scope.$on("$routeChangeSuccess",function(){
+		$scope.anim = false;		
+		action = $route.current.action;
 		requestService.setContext($route.current.action);
 
 		$scope.$broadcast('$requestContextChanged',requestService);
 	});	
+
+	$scope.$on("$requestContextChanged",function(){		
+		$scope.subview = requestService.getSection("");
+	});			
 	
 	dataService.getData("/3/movie/now_playing").success(function(data){			
 			$scope.data = data;
@@ -126,40 +162,52 @@ app.controller('frontCtrl',function($scope, $route, requestService, dataService)
 app.controller('standardCtrl',function($scope, $route, $routeParams, requestService){
 		$scope.subview = requestService.getSection('standard');
 		
+		
+		
 		$scope.$on("$requestContextChanged",function(){
 			$scope.subview = requestService.getSection('standard');
 		});			
 		
-		$scope.param = $routeParams.categoryId;
-		
-		
-		
+		$scope.param = $routeParams.categoryId;		
 });
 
 app.controller('latestCtrl',function($scope, $route, $routeParams, requestService){
-		$scope.subview = requestService.getSection('standard.latest');		
+		$scope.subview = requestService.getSection('standard.latest', $scope);		
 		
 		$scope.$on("$requestContextChanged",function(){
-			$scope.subview = requestService.getSection('standard.latest');
+			$scope.subview = requestService.getSection('standard.latest', $scope);
 		});			
 });
 
 
-app.controller('moviDetailsCtrl',function($scope, $route, $routeParams, requestService, dataService){
+app.controller('moviDetailsCtrl',function($scope, $route, $rootScope, $routeParams, requestService, dataService){		
 		$scope.subview = requestService.getSection('standard.latest.movi_details');		
-		
-		$scope.$on("$requestContextChanged",function(){
+		$scope.moviId = $routeParams.moviId;
+		$scope.$on("$requestContextChanged",function(){			
 			$scope.subview = requestService.getSection('standard.latest.movi_details');
-		});			
+		});					
+		dataService.getData("/3/movie/" + $routeParams.moviId + "/images").success(function(data){			
+			$rootScope.moviPosters = dataService.setPath(data, null, "w1000");			
+			//console.log($scope.moviPosters);			
+		});
 });
 
 app.controller('moviDetailPageCtrl',function($scope, $route, $routeParams, requestService, dataService){
 	dataService.getData("/3/movie/" + $routeParams.moviId).success(function(data){			
 			//$scope.moviDetails = data;			
 			$scope.moviDetails = dataService.setPath(data, null, "w500");
-			console.log(data);			
+			//console.log(data);			
 		});		
 });
 
+app.controller('moviPosterPageCtrl',function($scope, $route, $routeParams, requestService, dataService){
+				
+});
 
+app.controller('moviCastPageCtrl',function($scope, $route, $routeParams, requestService, dataService){
+	dataService.getData("/3/movie/" + $routeParams.moviId + "/credits").success(function(data){			
+			$scope.moviCast = dataService.setPath(data, null, "w92");						
+			//console.log($scope.moviReviews);			
+		});		
+});
 
